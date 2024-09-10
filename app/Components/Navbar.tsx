adbfb762064606821564c872f50994685bb30efc
yaseenmirshal <151372491+yaseenmirshal@users.noreply.github.com>
2024-09-10 19:54:59 +0530

    product list

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -26,11 +26,11 @@ const Navbar = () => {
         visible ? 'translate-y-0' : '-translate-y-full'
       } duration-500 ease-in-out z-50`}
       style={{  
-        backgroundColor: 'rgba(51, 51, 51, 0.8)', // Semi-transparent background color
-        backdropFilter: 'blur(10px)', // Blur effect
-        WebkitBackdropFilter: 'blur(10px)', // Safari support
-        borderBottom: '1px solid rgba(255, 255, 255, 0.18)', // Light border to enhance the glass effect
-        boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)' // Subtle shadow for depth
+        backgroundColor: 'white', // Semi-transparent background color
+        // backdropFilter: 'blur(10px)', // Blur effect
+        // WebkitBackdropFilter: 'blur(10px)', // Safari support
+        // borderBottom: '1px solid rgba(255, 255, 255, 0.18)', // Light border to enhance the glass effect
+        // boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)' // Subtle shadow for depth
       }}
     >
       <div className="mx-auto px-4 max-h-20">
@@ -38,7 +38,7 @@ const Navbar = () => {
           <div className="flex items-center">
             <Link href="/" legacyBehavior>
               <a className="flex items-center">
-                <img className='h-20' src="./whitelogojaza.png" alt="Jaza Perfumes Logo" />
+                <img className='h-14 mt-5 mb-5' src="./jaza white.png" alt="Jaza Perfumes Logo" />
               </a>
             </Link>
           </div>
@@ -71,7 +71,7 @@ const Navbar = () => {
               onClick={() => setIsOpen(!isOpen)}
             >
               <svg
-                className="w-6 h-6 text-lightGray hover:text-richGold"
+                className="w-6 h-6 text-black hover:text-richGold"
                 x-show="!isOpen"
                 fill="none"
                 strokeLinecap="round"
@@ -87,24 +87,24 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div  className={`md:hidden ${isOpen ? "block" : "hidden"}`} style={{ backgroundColor: '#333333' }}>
+      <div  className={`md:hidden flex  flex-col    ${isOpen ? "block" : "hidden"}`} style={{ backgroundColor: 'white' }}>
         <Link href="/" legacyBehavior>
-          <a className="block text-sm px-2 py-4 text-ivory font-semibold" style={{ color: '#FFFFF0' }}>Home</a>
+          <a className="block text-center  text-sm px-2 py-4 text-ivory font-semibold" style={{ color: 'black' }}>HOME</a>
         </Link>
         <Link href="/About" legacyBehavior>
-          <a className="block text-sm px-2 py-4 hover:text-richGold transition duration-300" style={{ color: '#D3D3D3' }}>About</a>
+          <a className="block text-sm px-2 py-4 hover:text-richGold transition duration-300" style={{ color: 'blacK' }}>ABOUT</a>
         </Link>
         <Link href="/Store" legacyBehavior>
-          <a className="block text-sm px-2 py-4 hover:text-richGold transition duration-300" style={{ color: '#D3D3D3' }}>Store</a>
+          <a className="block text-sm px-2 py-4 hover:text-richGold transition duration-300" style={{ color: 'BLACK' }}>STORE</a>
         </Link>
         <Link href="/Contact" legacyBehavior>
-          <a className="block text-sm px-2 py-4 hover:text-richGold transition duration-300" style={{ color: '#D3D3D3' }}>Contact</a>
+          <a className="block text-sm px-2 py-4 hover:text-richGold transition duration-300" style={{ color: 'black' }}>CONTACT</a>
         </Link>
         <div className="flex items-center justify-around py-4">
-          <FontAwesomeIcon icon={faHeart} className="text-lightgrey hover:text-richGold cursor-pointer" />
-          <FontAwesomeIcon icon={faShoppingCart} className="text-lightGray hover:text-richGold cursor-pointer" />
+          <FontAwesomeIcon icon={faHeart} className="text-black hover:text-richGold cursor-pointer" />
+          <FontAwesomeIcon icon={faShoppingCart} className="text-black hover:text-richGold cursor-pointer" />
           <Link href="/Signup">
-           <FontAwesomeIcon icon={faUser} className="text-lightGray hover:text-richGold cursor-pointer" /> 
+           <FontAwesomeIcon icon={faUser} className="text-black hover:text-richGold cursor-pointer" /> 
             </Link>
         </div>
        
