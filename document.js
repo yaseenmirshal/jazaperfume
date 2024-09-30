588b15138ebb16cd22a1c92297216c1c2b351bea
yaseenmirshal <151372491+yaseenmirshal@users.noreply.github.com>
2024-09-30 12:21:05 +0530

    google analytics

--- document.js
+++ document.js
@@ -10,6 +10,17 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap"
             rel="stylesheet"
           />
+          {/* Google tag (gtag.js) */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-SL96GE79Y7"></script>
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-SL96GE79Y7');
+            `}
+          </script>
         </Head>
         <body>
           <Main />
